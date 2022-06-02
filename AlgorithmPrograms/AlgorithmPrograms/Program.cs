287480f6a01ac4404af9672e94e51cb305ae11b4
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-02 16:04:42 +0530

    [Madhavi] all permutations of a string

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            String str = "ABC";
+            int n = str.Length;
+            Permutations.permute(str, 0, n - 1);
+        }
+    }
+}
