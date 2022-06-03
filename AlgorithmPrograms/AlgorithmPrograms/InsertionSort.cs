1ba108f013a34bb2150e51e59f3eefbdca101569
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-03 17:28:55 +0530

    [Madhavi] print string in sorted order using insertion sort

---
+++ AlgorithmPrograms/AlgorithmPrograms/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class InsertionSort
+    {
+        public static void sorting(String[] s, int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                String temp = s[i];
+
+                int j = i - 1;
+                while (j >= 0 && temp.Length < s[j].Length)
+                {
+                    s[j + 1] = s[j];
+                    j--;
+                }
+                s[j + 1] = temp;
+            }
+        }
+
+        public static void displayArrays(String[] str, int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(str[i] + " ");
+        }
+    }
+}
+
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -6,9 +6,24 @@ namespace AlgorithmPrograms
     {
         public static void Main(string[] args)
         {
-            String str = "ABC";
-            int n = str.Length;
-            Permutations.permute(str, 0, n - 1);
+            Console.WriteLine("1.calculate Permutations \n2.Insertion Sort");
+            Console.WriteLine("Enter your option:");
+            int option = Convert.ToInt32(Console.ReadLine());
+           
+            switch(option)
+            {
+                case 1:
+                    String str = "ABC";
+                    int n = str.Length;
+                    Permutations.permute(str, 0, n - 1);
+                    break;
+                case 2:
+                    string[] arr = { "Madhavi", "I", "am" };
+                    int size = arr.Length;
+                    InsertionSort.sorting(arr, size);
+                    InsertionSort.displayArrays(arr, size);
+                    break;
+            }
         }
     }
 }
